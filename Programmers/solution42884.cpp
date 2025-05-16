1730da8fa3072ea129179f16378e38fa7631b916
cheimer <ns1337@naver.com>
2025-05-16 09:26:10 +0900

    Programmers 42884
    단속카메라
    곂치는 부분이 있으면 범위를 줄이고 없으면 갯수를 늘리고 다음 영역으로 넘어감

---
+++ Programmers/solution42884.cpp
@@ -0,0 +1,51 @@
+﻿/*
+ * Link : https://school.programmers.co.kr/learn/courses/30/lessons/42884
+ *
+ * Solution : 모든 경로에 최소 하나씩은 포함되게 하는 배열의 최소값
+ *
+ *  routes : 경로 배열
+ *
+ *  1 <= routes.Length <= 10000
+ *  -30000 <= routes[i][0], routes[i][1] <= 30000
+ */
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes)
+{
+    for (auto& route : routes)
+    {
+        sort(route.begin(), route.end());
+    }
+
+    sort(routes.begin(), routes.end());
+
+    vector<int> CurrentRoute = routes[0];
+    int answer = 1;
+    
+    for (int i = 1; i < routes.size(); i++)
+    {
+        if (CurrentRoute[1] < routes[i][0])
+        {
+            answer++;
+            CurrentRoute = routes[i];
+        }
+        else
+        {
+            if (CurrentRoute[1] < routes[i][1])
+            {
+                CurrentRoute[0] = routes[i][0];
+            }
+            else
+            {
+                CurrentRoute = routes[i];
+            }
+        }
+    }
+
+    
+    return answer;
+}
