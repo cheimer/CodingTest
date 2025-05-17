392324a3e659a98d8d538e4383db57b02e25260c
cheimer <ns1337@naver.com>
2025-05-17 09:32:22 +0900

    Programmers 43105
    정수 삼각형
    
    DP 기본문제. 배열에 값을 저장하면서 진행

---
+++ Programmers/solution43105.cpp
@@ -0,0 +1,45 @@
+﻿/*
+ * Link : https://school.programmers.co.kr/learn/courses/30/lessons/43105
+ *
+ * Solution : 배열 중 하나를 선택한 후 다음 배열의 인접한 값을 더할 때 최대값을 찾음
+ *
+ *  triangle : 배열
+ *
+ *  1 <= triangle.size <= 500
+ *  1 <= triangle[i][j] <= 9999
+ *
+ */
+
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle)
+{
+    for (int i = 1; i < triangle.size(); i++)
+    {
+        for (int j = 0; j < triangle[i].size(); j++)
+        {
+            if (j == 0)
+            {
+                triangle[i][j] += triangle[i - 1][j];
+            }
+            else if (j == triangle[i].size() - 1)
+            {
+                triangle[i][j] += triangle[i - 1][j - 1];
+            }
+            else
+            {
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+            }
+        }
+    }
+
+    int answer = -1;
+    for (auto row : triangle[triangle.size() - 1])
+    {
+        answer = max(answer, row);
+    }
+    
+    return answer;
+}
